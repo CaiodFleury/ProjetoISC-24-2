e8e482928ac13a594a12b8da4afb95678b062fe2
OMassinha <caiodiasfleury@gmail.com>
2024-12-14 01:54:53 -0300

    Update main.asm

--- main.asm
+++ main.asm
@@ -2,76 +2,78 @@
 selectframeads:	.word 0xFF200604
 frame0:	     	.word 0xFF100000
 frame1: 	.word 0xFF000000
-.include "funcoes.asm"
+#.include "funcoes.asm"
 .include "fundolaranja.data"
+
 #Codigo vai começar na main.
 #Funções no final
-#Padrão for/while/if : loop_num
-#Padrão funções F.print
-#
-#
-#
+#Padrão for/while/if : Loop_num
+#Padrão funções FazerAlgo
+
 .text
 main:	
-	li  a0,4
-	li a7,1
-	ecall
-	li a0, 1 
-	call TrocarTela
 
+	la a0 fundolaranja
+	call LoadScreen
 	
-	li s0 0xFF100000
+	li a0, 3
+	call TrocarTela
 	
-	la t0 fundolaranja
-	lw t1 0(t0)
-	lw t2, 4(t0)
-	li t3, 0
-	mul t4, t1, t2
-	addi t0,t0,8
-	add  a0,t4,zero
-	li a7,1
-	ecall
-LOOP:	beq t3, t4, FORA
-	lw t5, 0(t0)
-	sw t5, 0(s0)
-	addi t0,t0,4
-	addi t3,t3,4
-	addi s0,s0,4
-	li  a0,2
-	li a7,1
-	ecall
-	j LOOP
-FORA:
-	li s1, 1
-	lw s0, selectframeads
-	sw s1, 0(s0)
-	li a7,10
-	ecall	 
+	call FimPrograma	 
+	
+	
 #FUNÇÕES--->	
-#recebe a0; a = 0/1 define a tela a = 3 troca 
+#recebe a0; a0 = 0/1 define a tela a0 = 3 troca 
 TrocarTela:
-	lw s0, selectframeads
+	lw t3, selectframeads
  	If_TT1: 
  		li t0,3
  		bge a0,t0, Else_TT1
-		sw a0, 0(s0)
+		sw a0, 0(t3)
 		ret
 	Else_TT1:
-		lb t1,0(s0)
+		lb t1,0(t3)
 		li t0, 1
 		If_TT2:
 			bne t1,t0, Else_TT2
 			li t1, 0
-			sw t1, 0(s0)
+			sw t1, 0(t3)
 			ret
 		Else_TT2:
 			li t1, 1
-			sw t1, 0(s0)
+			sw t1, 0(t3)
 			ret
+#recebe a0,a1,a2; a0= endereço imagem, a1 = x da imagem, a2 = y da imagem
+LoadScreen:
+	lw t3 0(a0)
+	lw t4,4(a0)
+	mul t3, t3, t4 #pega o total de pixels
+	li t4, 0
+	lw t2, selectframeads
+	lb t2,0(t2)
+ 	If_LS1: 
+ 		li t0,1
+ 		bne t2,t0, Else_LS1
+ 		li t2 0xFF000000
+		j Fim_If_LS1
+	Else_LS1:
+		li t2 0xFF100000
+	Fim_If_LS1:
+	addi a0,a0,8
+	While_LS1:	
+	beq t3, t4, EndWhile_LS1
+		lw t5, 0(a0)
+		sw t5, 0(t2)
+		addi t4,t4,4
+		addi a0,a0,4
+		addi t2,t2,4
+		j While_LS1
+	EndWhile_LS1:
+	ret
 	
-	
-	
-	
+FimPrograma:
+	li a7,10
+	ecall
 	
 	
 	
