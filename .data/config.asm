93e8f9479c04a2bcea0468fa13eb2236c1bb565f
Cid <65530477+CaiodFleury@users.noreply.github.com>
2025-01-18 00:24:34 -0300

    Update config.asm

--- .data/config.asm
+++ .data/config.asm
@@ -6,6 +6,17 @@ old_char_pos:	.half 200, 0
 arrow_pos:	.half 4
 indio_pos:	.half 48, 100
 char_pos_bounds:.half 80, 240, 232, 64
-garden_matriz_x:.half 72,116, 160 , 204, 248
-garden_matriz_y:.half 160,120, 80
 selectframeads:	.word 0xFF200604
+garden_matriz_x:.half 72, 116, 160, 204, 248
+garden_matriz_y:.half 160,120, 80
+
+# a logica se consiste na equacao:
+# 72 + (44 * x)
+# 160 - (40 * y)
+# desse modo, podemos saber a coordenada do jardim de acordo com esse indice
+current_garden_x:.byte 0
+current_garden_y:.byte 0
+
+# matriz que representa o estado do jardim (quao regado ele esta)
+garden_state_x: .byte 0, 0, 0, 0, 0
+garden_state_y: .byte 0, 0, 0
