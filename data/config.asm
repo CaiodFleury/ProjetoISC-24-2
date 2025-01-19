25746d964b23075c0c78a07783c7113103777d14
OMassinha <caiodiasfleury@gmail.com>
2025-01-19 03:16:12 -0300

    organização 1

--- data/config.asm
+++ data/config.asm
@@ -9,6 +9,7 @@ char_pos_bounds:.half 84, 240, 232, 64
 selectframeads:	.word 0xFF200604
 garden_matriz_x:.half 88, 124, 160, 196, 232
 garden_matriz_y:.half 148,120, 92
+var: .word 28
 
 # a logica se consiste na equacao:
 # 88 + (36 * x)
