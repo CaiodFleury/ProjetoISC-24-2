eb7245096562a9e28cbbe3a2b81a2f7ddc5668df
CorsaRebaixado <53981523+CorsaRebaixado@users.noreply.github.com>
2025-01-20 22:18:05 -0300

    Add files via upload

---
+++ funcoes.asm
@@ -0,0 +1,498 @@
+#FUNCOES--->
+TocarMusica:						#s11 eh o contador de tempo
+	li a7,30					#coloca o horario atual em a0
+	ecall						#fun��o n�o recebe entrada
+ 	If_TM:						#apenas toca a proxima nota de Notas
+ 		blt a0,s11, Fim_If_TM
+		la t2,Music_config
+ 		lw t0, 0(t2)
+ 		lw t1, 4(t2)
+ 		lw a2, 8(t2)
+ 		lw a3, 12(t2)
+		If_TM1:
+			bne t0,t1, Fim_If_TM1	# contador chegou no final? entÃ£o  vÃ¡ para SET_SONG para zerar o contador e as notas (loop infinito)
+			sw zero, 4(t2)
+			li t1, 0
+		Fim_If_TM1:
+		la t4, Notas
+		li t3,8
+		mul t1, t1,t3
+		add t4,t4,t1
+		lw a0,0(t4)		# le o valor da nota
+		lw a1,4(t4)		# le a duracao da nota
+		li a7,31		# define a chamada de syscall
+		ecall			# toca a nota
+		
+		li a7,30		# coloca o horario atual em a0
+		ecall
+		
+		mv a0,s11
+		lw t4, 4(t4)
+		add s11,s11,t4
+
+		lw t6, 4(t2)
+		addi t6,t6,1
+		sw t6,4(t2)		# incrementa o contador de notas
+	Fim_If_TM:
+	ret
+
+TrocarTela:					#recebe a0	 
+	lw t3, selectframeads 			# a0 = 0/1 define a tela, a0 = 2 troca
+ 	If_TT1: 					 	
+ 		li t0,2				#recebe o valor da tela atual 
+ 		bge a0,t0, Else_TT1		#If a0 < 2 troca tela para a0
+		sw a0, 0(t3)
+		ret
+	Else_TT1:				#Se nao, se a tela atual ÃƒÂ© 1 troca para 0
+		lb t1, 0(t3)			#se a tela for 0 troca para 1 vice-versa
+		xori t1,t1,1
+		sb t1,0(t3) 
+	ret
+
+LoadScreen:						#Recebe a0, a1, a2;
+	lw t0, selectframeads				# a0= endereco imagem
+	lb t0,0(t0)					# a1 = x da imagem
+ 	If_LS: 						# a2 = y da imagem
+ 		li t1,1					
+ 		bne t0,t1, Else_LS
+ 		li t0 0xFF000000
+		j Fim_If_LS
+	Else_LS:
+		li t0 0xFF100000
+	Fim_If_LS:
+	li t5 , 320
+	mul t5 , t5, a2 
+	add t0, t5 , t0
+	
+	lw t1, 0(a0)
+	lw t2, 4(a0)
+	li t3, 0
+	li t4, 0
+	addi a0,a0,8
+	While_LS:	
+		beq t2, t4, EndWhile_LS
+		add t0, t0,a1
+		While_LS1:
+			beq t1, t3, EndWhile_LS1
+			lw t5, 0(a0)
+			sw t5, 0(t0)
+			addi t3,t3,4
+			addi a0,a0,4
+			addi t0,t0,4
+			j While_LS1
+		EndWhile_LS1:
+		li t5, 320
+		sub t5, t5, a1
+		sub t5, t5,t1
+		add t0, t0, t5
+		li t3, 0
+		addi t4, t4,1
+		j While_LS
+	EndWhile_LS:
+	ret
+	
+UnloadImage:						# a0= endereco imagem
+	li t0, 76800					# a1 = x da imagem
+	mul t0, t0, a3					# a2 = y da imagem
+	la t1 , array_layers				# a3 = layer(0,5)
+	add t0 , t0, t1
+	li t1 , 320
+	mul t1,a2,t1
+	add t0,t0,t1					
+ 	lw t2, 0(a0)
+	lw t3, 4(a0)
+	li t4, 0
+	li t5, 0
+	li t6 ,0xC7C7C7C7
+	While_UI:	
+		beq t3, t4, EndWhile_UI
+		add t0, t0,a1
+		While_UI1:
+			beq t2, t5, EndWhile_UI1
+			sw t6, 0(t0)
+			addi t5,t5,4
+			addi t0,t0,4
+			j While_UI1
+		EndWhile_UI1:
+		li t5, 320
+		sub t5, t5, a1
+		sub t5, t5,t2
+		add t0, t0, t5
+		li t5, 0
+		addi t4, t4,1
+		j While_UI
+	EndWhile_UI:					
+ 	ret						
+
+LoadImage:						# a0= endereco imagem
+	li t0, 76800					# a1 = x da imagem
+	mul t0, t0, a3					# a2 = y da imagem
+	la t1 , array_layers				# a3 = layer(0,5)
+	add t0 , t0, t1	
+	li t1 , 320
+	mul t1,a2,t1
+	add t0,t0,t1				
+ 	lw t2, 0(a0)
+	lw t3, 4(a0)
+	li t4, 0
+	li t5, 0
+	addi a0,a0,8
+	While_LI:	
+		beq t3, t4, EndWhile_LI
+		add t0, t0,a1
+		While_LI1:
+			beq t2, t5, EndWhile_LI1
+			lw t6, 0(a0)
+			sw t6, 0(t0)
+			addi t5,t5,4
+			addi a0,a0,4
+			addi t0,t0,4
+			j While_LI1
+		EndWhile_LI1:
+		li t5, 320
+		sub t5, t5, a1
+		sub t5, t5, t2
+		add t0, t0, t5
+		li t5, 0
+		addi t4, t4,1
+		j While_LI
+	EndWhile_LI:
+	ret	
+
+LoadAnimation:						# a0= endereco imagem
+	li t0, 76800					# a1 = x da imagem
+	mul t0, t0, a3					# a2 = y da imagem
+	la t1 , array_layers				# a3 = layer(0,5)
+	add t0 , t0, t1					# a4 = jump x
+	li t1 , 320
+	mul t1,a2,t1
+	add t0,t0,t1
+	mv t1,a0				
+ 	li t2, 320
+	li t3, 240
+	li t4, 0
+	li t5, 0
+	addi a0,a0,8
+	While_LA:	
+		beq t3, t4, EndWhile_LA
+		add t0, t0,a1
+		add a0, a0,a4
+		While_LA1:
+			beq t2, t5, EndWhile_LA1
+			lw t6, 0(a0)
+			sw t6, 0(t0)
+			addi t5,t5,4
+			addi a0,a0,4
+			addi t0,t0,4
+			j While_LA1
+		EndWhile_LA1:
+		li t5, 320
+		sub t5, t5, a1
+		sub t5, t5, t2
+		add t0, t0, t5
+		
+		lw t5,0(t1)
+		sub t5,t5,t2
+		add a0,a0,t5
+		sub a0,a0,a4
+		li t5, 0
+		addi t4, t4,1
+		j While_LA
+	EndWhile_LA:
+	ret
+
+
+Renderizador:
+	#le o frame atual e pega o outro para modificar
+	#array layers Ã© a memoria das camadas
+	#
+	lw t0, selectframeads				#a4 = atualizar tela ou nao
+	lb t0, 0(t0)					#x1 < x2, y1 < y2
+	xori t0,t0,1					#t0 eh o endereco da tela
+	li t1, 0xFF0					#t1/t2/t3/t4 sao os contadores
+	add t0 ,t0,t1					#t5 eh usado como valor temporario a todo momento
+	slli t0 ,t0, 20					#t6 eh o endereco na memoria
+	################				#ESTOU USANDO a7 e a6 COMO TEMP			
+	la t6, array_layers				
+	li t1,320
+	li t2,240									
+	li t3, 0
+	li t4, 0
+	li a6, 0xffffffC7
+	While_R:	
+		beq t1, t4, EndWhile_R
+		While_R1:
+			beq t2, t3, EndWhile_R1
+			li t5,384000
+			add a7, t6, t5
+			While_R2:
+				lb t5,0(a7)
+				bne t5, a6,EndWhile_R2
+				la t5 , array_layers 
+				blt a7,t5,EndWhile_R3
+				li t5, 76800
+				sub a7, a7,t5
+				j While_R2
+			EndWhile_R3:
+			li t5, 0
+			EndWhile_R2:
+			sb t5, 0(t0)
+			addi t3,t3,1
+			addi t0,t0,1
+			addi t6,t6,1
+			j While_R1
+		EndWhile_R1:
+		li t3, 0
+		addi t4, t4,1
+		j While_R
+	EndWhile_R:
+	lw t0, selectframeads	
+	lb t1, 0(t0)			
+	xori t1,t1,1
+	sb t1,0(t0) 
+	ret
+# deixei aqui o renderizador que le baseado em a0, a1, a2 e a3, se for usar teste, pois nao conferi codigo
+Renderizador2:
+	#le o frame atual e pega o outro para modificar
+	#array layers Ã© a memoria das camadas
+	#
+	lw t0, selectframeads	
+	lb t0, 0(t0)					#a0 = x0
+	xori t0,t0,1					#a1 = y0
+	li t1, 0xFF0					#a2 = x1
+	add t0 ,t0,t1					#a3 = y1
+	slli t0 ,t0, 20
+	################				#a4 = atualizar tela ou nao				
+	la t6, array_layers				#x1 < x2, y1 < y2
+	li t5 , 320					#t0 eh o endereco da tela
+	mul t5 , t5, a1					#t1/t2/t3/t4 sao os contadores
+	add t0, t5 , t0					#t5 eh usado como valor temporario a todo momento
+	add t6, t5,t6					#t6 eh o endereco na memoria
+	sub t1,a3,a1					#ESTOU USANDO a7 e a6 COMO TEMP
+	sub t2,a2,a0
+	li t3, 0
+	li t4, 0
+	li a6, 0xffffffC7
+	While_2R:	
+		beq t1, t4, EndWhile_2R
+		add t0, t0,a0
+		add t6, t6,a0
+		While_2R1:
+			beq t2, t3, EndWhile_2R1
+			add a7, t6, zero
+			li t5,384000
+			add a7, a7,t5
+			While_2R2:
+				lb t5,0(a7)
+				bne t5, a6,EndWhile_2R2
+				la t5 , array_layers 
+				blt a7,t5,EndWhile_2R3
+				li t5, 76800
+				sub a7, a7,t5
+				j While_2R2
+			EndWhile_2R3:
+			li t5, 0
+			EndWhile_2R2:
+			sb t5, 0(t0)
+			addi t3,t3,1
+			addi t0,t0,1
+			addi t6,t6,1
+			j While_2R1
+		EndWhile_2R1:
+		li t5, 320
+		sub t5, t5, a2
+		add t0, t0, t5
+		add t6,t6,t5
+		li t3, 0
+		addi t4, t4,1
+		j While_2R
+	EndWhile_2R:
+	beq a4, zero , Fim_2R
+	lw t0, selectframeads	
+	lb t1, 0(t0)			
+	xori t1,t1,1
+	sb t1,0(t0) 
+	Fim_2R:ret
+
+WaterGarden:
+		# t2 era minha posição
+		mv a0,t2
+		# essa parte vai salvar o estado do jardim atual
+		la t0, garden_state
+		add t1, t0, a0 			# move o ponteiro para o indice correto
+		lb a1, 0(t1)
+		addi a1, a1, 1
+		sb a1, 0(t1)			# salva o novo valor
+		
+		li t0,5
+		rem t5,a0,t0
+		divu t6,a0,t0
+		# t5 = x_garden_state
+		# t6 = y_garden_state
+
+		# modificando sprite da plantacao
+		li t0, 36
+		li t1, 28
+		li t2, 92
+		# 88 + (36 * x)
+		
+		mul t4, t5, t0
+		addi t5, t4, 88
+
+		# 92 + (28 * y)
+		mul t4, t6, t1
+		add t6, t2, t4
+
+		# t5 = garden_x
+		# t6 = garden_y
+
+		li t0, 1
+		li t1, 2
+		li t3, 3
+
+		beq a1, t0, PLANTA1
+		beq a1, t1, PLANTA2
+		beq a1, t3, PLANTA3
+
+		PLANTA1:
+			la a0, planta1
+			j PLANTAR
+
+		PLANTA2:
+			la a0, planta2
+			j PLANTAR
+
+		PLANTA3:
+			la a0, planta3
+			j PLANTAR
+
+		VOLTAR:
+			ret
+
+		PLANTAR:
+			mv a1, t5 # x da imagem
+			mv a2, t6 # y da imagem
+			li a3, 4 # layer 4
+			call LoadImage
+
+		j GAME_LOOP	
+
+AnimationScreen:
+	Loop_AS:
+	
+	call TocarMusica
+	
+	la t0, old_char_pos
+	lh a1 , 0(t0)
+	lh a2 , 2(t0)
+	li a3 , 5
+	la a0 macaco
+	call UnloadImage
+	
+	la t0, char_pos
+	lh a1 , 0(t0)
+	lh a2 , 2(t0)
+	li a3 , 5
+	la a0 ,macaco
+	call LoadImage
+	
+	#
+	la t0, old_indio_pos
+	lh a1 , 0(t0)
+	lh a2 , 2(t0)
+	li a3 , 5
+	la a0 inimigo
+	call UnloadImage
+	
+	la t0, indio_pos
+	lh a1 , 0(t0)
+	lh a2 , 2(t0)
+	li a3 , 5
+	la a0 ,inimigo
+	call LoadImage
+	#
+
+	la t0, var
+	la a0 fazendav2
+	li a1 , 0
+	li a2 , 0
+	li a3 , 3
+	lw a4,0(t0)
+	call LoadAnimation
+	
+	call Renderizador
+	
+	la t0, var
+	lw t2, 0(t0)
+	li t1, 160
+	beq t1,t2,FimAnimacaoUm
+	addi t2,t2,4
+	sw t2,0(t0)
+	
+	la t0, char_pos
+	la t1, old_char_pos
+	lh t2 , 0(t0)
+	sh t2 , 0(t1)
+	addi t2,t2,-4
+	sh t2 , 0(t0)
+	
+	#
+	la t0, indio_pos
+	la t1, old_indio_pos
+	li t3, 1
+	sh t3, 4(t0)
+	lh t2, 0(t0)
+	sh t2, 0(t1)
+	addi t2, t2, -4
+	sh t2, 0(t0)
+	
+	#li t3, 160
+	#sh t3, 0(t0)
+	#
+	
+	li a7, 32
+	li a0, 20
+	ecall
+	
+	j Loop_AS
+	
+	
+Inimigo:
+	
+	#li a0, 200
+	#li a7, 32
+	#ecall
+
+	la t0, indio_pos
+	la t1, old_indio_pos
+	lh a1, 0(t0)
+	sh a1, 0(t1)
+	li t3, 232
+	li t4, 88
+	li t5, -1
+	lh t6, 4(t0)
+	
+	beq t6, t5, Esq
+	
+Dir:	beq a1, t3, Reset
+	addi a1, a1, 4
+	sh a1, 0(t0)
+	sh t6, 4(t0)
+	j Back1
+	
+Esq:	beq a1, t4, Reset
+	addi a1, a1, -4
+	sh a1, 0(t0)
+	sh t6, 4(t0)
+	j Back1
+	
+Reset:
+	mul t6, t6, t5
+	sh t6, 4(t0)
+	j Back1
+	
+
+
+FimPrograma:			#Nao recebe nada
+	li a7,10      		#Chama o procedimento de finalizar o programa
+	ecall			#Nao retorna nada
--- main.asm
+++ main.asm
@@ -1,74 +1,78 @@
 .data
-selectframeads:	.word 0xFF200604
-char_pos:	.half 80, 176
-old_char_pos:	.half 80, 176
-char_pos_bounds:.half 80, 240, 232, 64
-#array_layers:	.byte 0xC7:460800
-Music_config: 	.word 260,0,29,121 #notas total, nota atual, instrumento, volume
-Notas: 36,202,36,202,48,202,36,202,36,202,47,202,36,202,36,202,45,202,36,202,36,202,42,202,36,202,36,202,43,202,45,202,36,202,36,202,48,202,36,202,36,202,47,202,36,202,36,202,45,202,36,202,36,202,42,1015,36,202,36,202,48,202,36,202,36,202,47,202,36,202,36,202,45,202,36,202,36,202,42,202,36,202,36,202,43,202,45,202,36,202,36,202,48,202,36,202,36,202,47,202,36,202,36,202,67,100,64,100,60,100,64,100,67,100,64,100,67,100,72,100,67,100,64,100,67,100,64,100,67,100,72,100,76,100,79,100,41,202,41,202,53,202,41,202,41,202,52,202,41,202,41,202,50,202,41,202,41,202,49,202,41,202,41,202,48,202,50,202,41,202,41,202,53,202,41,202,41,202,52,202,41,202,41,202,50,202,41,202,41,202,49,1015,41,202,41,202,53,202,41,202,41,202,52,202,41,202,41,202,50,202,41,202,41,202,49,202,41,202,41,202,48,202,50,202,41,202,41,202,53,202,41,202,41,202,52,202,41,202,41,202,65,100,61,100,60,100,65,100,60,100,57,100,60,100,65,100,69,100,65,100
-.include "levels/array_layers.data"
-.include "levels/map_placeholder.s"
-.include "levels/Predio.data"
-.include "sprites/chapoca.data"
-.include "sprites/fundopersonagem.data"
-.include "sprites/personagem.data"	
+.include "data/imagens.asm"
+.include "data/config.asm"
+.text
 #Codigo vai comecar na main.
 #Funcoes no final
 #Padrao for/while/if : Loop_num
 #Padrao funcoes FazerAlgo
+#UNICA VARIAVEL GLOBAL … S11 q È o tempo atual
+main:	
+	
+	call StartScreen
+	FimStartScreen:
+	
+	#colocar anima√ß√£o de introdu√ß√£o aqui
+			
+	call LoadGame
+	FimGame:
+	
+	call FimPrograma
+			
+#tela de inicio
+StartScreen:
+	li a1 , 0
+	li a2 , 0
+	la a0 ,startscreen
+	call LoadScreen
+	
+	li a0, 2
+	call TrocarTela	
+	
+	li a1 , 100
+	li a2 , 100
+	la a0 Loading
+	call LoadScreen
+	
+	li a0,0xFF200000		# carrega o endereco de controle do KDMMIO
+	lw t0,0(a0)			# Le bit de Controle Teclado
+	andi t0,t0,0x0001		# mascara o bit menos significativo
+   	beq t0,zero,StartScreen  	# Se nao ha tecla pressionada entao vai para FIM			
+	
+	j FimStartScreen
 
-.text
-main:		
+#Administrador maximo do jogo
+LoadGame:
 	li a7,30		# coloca o horario atual em s11
 	ecall
 	add s11 , zero , a0
-	li a0, 0
-	call TrocarTela			
-	# s7 = x_bounds_1
-	# s8 = x_bounds_2
-	# s9 = y_bounds 1
-	# s10 = y_bounds 2
 	
-	# O carregamento eh feito aqui para poupar processamento posterior
-	##########################
-	la t0, char_pos_bounds
-	
-	lh s7, 0(t0) # x1
-	lh s8, 2(t0) # x2
-	lh s9, 4(t0) # y1
-	lh s10, 6(t0)# y2
-	
-	##########################
-	
+	#Primeira parte do nivel
+	li a1 , 0
+	li a2 , 0
+	li a3 , 3
+	la a0 fazendav1
+	call LoadImage
 	
 	li a1 , 0
 	li a2 , 0
-	li a3 , 4
-	la a0 Predio
+	li a3 , 0
+	la a0 colisao1
 	call LoadImage
 	
-	li a0 , 0
+	call GAME_LOOP
+	#Segunda parte do nivel
+	SegundaParte:
+	call AnimationScreen
+	FimAnimacaoUm:
+	
 	li a1 , 0
-	li a2 , 320
-	li a3 , 240
-	li a4 , 1
-	call Renderizador
-
-	la t0, char_pos
-	lh a1 , 0(t0)
-	lh a2 , 2(t0)
-	li a3 , 5
-	la a0 chapoca
+	li a2 , 0
+	li a3 , 0
+	la a0 colisao2
 	call LoadImage
 	
-	li a0 , 0
-	li a1 , 0
-	li a2 , 320
-	li a3 , 240
-	li a4 , 1
-	call Renderizador
-	
-	#call FimPrograma		
+	call GAME_LOOP
 	
 #O game loop vai ser responsavel por administrar:
 #efeitos visuais e coisas que mudam na tela
@@ -76,57 +80,70 @@ main:
 #Modificacoes chamarao a renderizacao	
 GAME_LOOP: 	
 
-	li t6 0
 	li a0,0xFF200000		# carrega o endereco de controle do KDMMIO
 	lw t0,0(a0)			# Le bit de Controle Teclado
 	andi t0,t0,0x0001		# mascara o bit menos significativo
    	beq t0,zero,PularKeyDown   	# Se nao ha tecla pressionada entao vai para FIM			
 	call KeyDown
-	li t6, 1
 	PularKeyDown:
+			
+	#renderizaÁ„o
+	la t0, old_char_pos
+	lh a1 , 0(t0)
+	lh a2 , 2(t0)
+	li a3 , 5
+	la a0 macaco
+	call UnloadImage
 	
-	# << local para colocar mudancas no cenario
+	la t0, char_pos
+	lh a1 , 0(t0)
+	lh a2 , 2(t0)
+	li a3 , 5
+	la a0 , macaco
+	call LoadImage
 	
-	beq  t6,zero,PularRenderizar
-	Renderizar:
-		la t0, char_pos
-		lh a1 , 0(t0)
-		lh a2 , 2(t0)
-		li a3 , 5
-		la a0 chapoca
-		call LoadImage
 	
-		la t0, char_pos
-		la a0 chapoca
-		li a4 , 1
-		lw a2 , 0(a0)
-		lw a3 , 4(a0)
-		lh a0 , 0(t0)
-		lh a1,  2(t0)
-		add a3 ,a3 ,a1
-		add a2 ,a2 ,a0
-		call Renderizador
-		
-		la t0, old_char_pos
-		lh a1 , 0(t0)
-		lh a2 , 2(t0)
-		li a3 , 5
-		la a0 chapoca
-		call UnloadImage
-		
-		la t0, old_char_pos
-		la a0 chapoca
-		li a4 , 0
-		lw a2 , 0(a0)
-		lw a3 , 4(a0)
-		lh a0 , 0(t0)
-		lh a1,  2(t0)
-		add a3 ,a3 ,a1
-		add a2 ,a2 ,a0
-		call Renderizador
-	PularRenderizar:
+	la t0, indio_pos
+	lh t1, 4(t0)
+	beq t1, zero, Back2
 	
-		call TocarMusica #CHAMA A MUSICA. COLOQUEI AKI PQ FOI O LUGAR Q O DESEMPENHO FICOU MELHOR
+	call Inimigo
+	
+Back1:	#
+	
+	la t0, old_indio_pos
+	lh a1 , 0(t0)
+	lh a2 , 2(t0)
+	li a3 , 5
+	la a0 inimigo
+	call UnloadImage
+	
+	la t0, indio_pos
+	lh a1 , 0(t0)
+	lh a2 , 2(t0)
+	li a3 , 5
+	la a0 , inimigo
+	call LoadImage
+	
+	#
+Back2:
+	la t0, indio_pos
+	lh t1, 4(t0)
+	bne t1, zero, Skip
+	la t0, indio_pos
+	lh a1 , 0(t0)
+	lh a2 , 2(t0)
+	li a3 , 5
+	la a0 , inimigo
+	call LoadImage
+	#
+	
+Skip:
+	#
+	
+	call Renderizador
+	
+	#call TocarMusica #CHAMA A MUSICA. COLOQUEI AKI PQ FOI O LUGAR Q O DESEMPENHO FICOU MELHOR
 		
 	j GAME_LOOP
 
@@ -134,6 +151,29 @@ KeyDown:
 	
   	lw t2,4(a0)  			# le o valor da tecla tecla
 		
+	#Variaveis para atingir o ponto atual
+	la t6,array_layers
+	la t5, char_pos
+	lh t3, 0(t5)
+	lh t4, 2(t5)
+	#operaÁıes para chegar no ponto certo
+	add t6,t6,t3
+	li t3,320
+	mul t4,t4,t3
+	add t6,t4,t6
+	li t4,8640
+	add t6,t6,t4
+	# t6 recebe o valor do pixel na tela desejado q È o ponto esquerdo inferior
+	
+	li t0, 'e'
+	beq t2,t0, SpaceInteraction	
+	
+	#Troca a posiÁ„o antiga com a atual e carrega t5 com char_pos
+	la t5, char_pos
+	la t4, old_char_pos
+	lw t3, 0(t5)
+	sw t3, 0(t4)
+	
 	li t0, 'd'
 	li t1, 'D'
 	beq t2, t0, MoveRight
@@ -153,302 +193,142 @@ KeyDown:
 	li t1, 'S'
 	beq t2, t0, MoveDown
 	beq t2, t1, MoveDown
+	
+	li t0, 'm'
+	li t1, 'M'
+	beq t2, t0, MoveUpRight
+	beq t2, t1, MoveUpRight
+	
+	li t0, 'j'
+	li t1, 'J'
+	beq t2, t0, MoveUpLeft
+	beq t2, t1, MoveUpLeft
+		
+	li t0, 'k'
+	li t1, 'K'
+	beq t2, t0, MoveDownRight
+	beq t2, t1, MoveDownRight
+	
+	li t0, 'n'
+	li t1, 'N'
+	beq t2, t0, MoveDownLeft
+	beq t2, t1, MoveDownLeft
 
 	FIM:	ret				# retorna
 	
+	SpaceInteraction:
+		lb t2,0(t6)
+		li t3, 20
+		bltu t2,t3,WaterGarden
+		ret
 	
-	# t1 = x, t2 = y
 	MoveRight:
-		la t0, char_pos
-		la t1, old_char_pos
-		lw t2, 0(t0)
-		sw t2, 0(t1)
-		lh t1, 0(t0)
-		
-		# bge t0,t1,Label # t0>=t1 ? PC=Label : PC=PC+4 t0 e t1 sem sinal
 
-		# blt t0,t1,Label # t0<t1 ? PC=Label : PC=PC+4 
-		
-		addi t5, t1, 32
-		bge t5, s8, FIM # se o prÛximo passo vai sair do limite, vai ir para RETURN
-		
-		addi t1, t1, 32 # 32 bits pra direita
-		sh t1, 0(t0)
+		addi t6,t6,4
+		lb t2,0(t6)
+		li t3,-110
+		beq t2,t3,FIM # se o pixel for azul ele n„o se meche
+		li t3,63
+		beq t2,t3,SegundaParte # se for amarelo ele vai para a segunda parte do mapa
+		lh t2, 0(t5)
+		addi t2, t2,4
+		sh t2, 0(t5)
 		ret
 		
 	MoveLeft:
-		la t0, char_pos
-		la t1, old_char_pos
-		lw t2, 0(t0)
-		sw t2, 0(t1)
-		lh t1, 0(t0)
+		addi t6,t6,-4
 		
-		addi t5, t1, -32
-		blt t5, s7, FIM # se o prÛximo passo vai sair do limite, vai ir para RETURN
+		lb t2,0(t6)
+		li t6,-110
+		beq t2,t6,FIM
 		
-		addi t1, t1, -32 # 32 bits pra esquerda
-		sh t1, 0(t0)
+		lh t1,0(t5)
+		addi t1, t1, -4
+		sh t1, 0(t5)
 		ret
 		
-	# s7 = x_bounds_1
-	# s8 = x_bounds_2
-	# s9 = y_bounds 1
-	# s10 = y_bounds 2
 	MoveUp:
-		la t0, char_pos
-		la t1, old_char_pos
-		lw t2, 0(t0)
-		sw t2, 0(t1)
-		lh t1, 2(t0)
+		addi t6,t6,-1280
 		
-		addi t5, t1, -56
-		blt t5, s10, FIM
-		
-		addi t1, t1, -56 # 56 bits pra esquerda
-		sh t1, 2(t0)
+		lb t4,0(t6)
+		li t6,-110
+		beq t4,t6,FIM
+
+		lh t1, 2(t5)
+		addi t1, t1, -4 
+		sh t1, 2(t5)
 		ret
 		
 	MoveDown:
-		la t0, char_pos
-		la t1, old_char_pos
-		lw t2, 0(t0)
-		sw t2, 0(t1)
-		lh t1, 2(t0)
+
+		addi t6,t6,1280
 		
-		addi t5, t1, 56
-		bge t5, s9, FIM
-		
-		addi t1, t1, 56 # 56 bits pra esquerda
-		sh t1, 2(t0)
+		lb t4,0(t6)
+		li t6,-110
+		beq t4,t6,FIM
+
+		lh t1, 2(t5)
+		addi t1, t1, 4 
+		sh t1, 2(t5)
 		ret
-
-#FUNCOES--->	
-
-TocarMusica:#s11 ? o contador
-	li a7,30		# coloca o horario atual em a0
-	ecall
- 	If_TM:
- 		blt a0,s11, Fim_If_TM
-		la t2,Music_config
- 		lw t0, 0(t2)
- 		lw t1, 4(t2)
- 		lw a2, 8(t2)
- 		lw a3, 12(t2)
-		If_TM1:
-			bne t0,t1, Fim_If_TM1	# contador chegou no final? ent?o  v? para SET_SONG para zerar o contador e as notas (loop infinito)
-			sw zero, 4(t2)
-			li t1, 0
-		Fim_If_TM1:
-		la t4, Notas
-		li t3,8
-		mul t1, t1,t3
-		add t4,t4,t1
-		lw a0,0(t4)		# le o valor da nota
-		lw a1,4(t4)		# le a duracao da nota
-		li a7,31		# define a chamada de syscall
-		ecall			# toca a nota
 		
-		li a7,30		# coloca o horario atual em a0
-		ecall
-		
-		mv s11, a0
-		lw t4, 4(t4)
-		add s11,s11,t4
-
-		lw t6, 4(t2)
-		addi t6,t6,1
-		sw t6,4(t2)		# incrementa o contador de notas
-	Fim_If_TM:
-	ret
-
-TrocarTela:					#recebe a0	 
-	lw t3, selectframeads 			# a0 = 0/1 define a tela, a0 = 2 troca
- 	If_TT1: 					 	
- 		li t0,2				#recebe o valor da tela atual 
- 		bge a0,t0, Else_TT1		#If a0 < 2 troca tela para a0
-		sw a0, 0(t3)
+	MoveUpRight:
+		addi t6,t6,-1276
+		lb t2,0(t6)
+		li t3,-110
+		beq t2,t3,FIM # se o pixel for azul ele n„o se meche
+		li t3,63
+		beq t2,t3,SegundaParte # se for amarelo ele vai para a segunda parte do mapa
+		lh t2, 0(t5)
+		addi t2, t2,4
+		sh t2, 0(t5)
+		lh t2, 2(t5)
+		addi t2, t2,4
+		sh t2, 2(t5)
 		ret
-	Else_TT1:				#Se nao, se a tela atual È 1 troca para 0
-		lb t1,0(t3)			#se a tela È 0 troca para 1
-		li t0, 1
-		If_TT2:
-			bne t1,t0, Else_TT2
-			li t1, 0
-			sw t1, 0(t3)
-			ret
-		Else_TT2:
-			li t1, 1
-			sw t1, 0(t3)
-	ret
-
-LoadScreen:						#Recebe a0, a1, a2;
-	lw t0, selectframeads				# a0= endereco imagem
-	lb t0,0(t0)					# a1 = x da imagem
- 	If_LS: 						# a2 = y da imagem
- 		li t1,1					
- 		bne t0,t1, Else_LS
- 		li t0 0xFF000000
-		j Fim_If_LS
-	Else_LS:
-		li t0 0xFF100000
-	Fim_If_LS:
-	li t5 , 320
-	mul t5 , t5, a2 
-	add t0, t5 , t0
+		
+	MoveDownRight:
+		addi t6,t6,1284
+		lb t2,0(t6)
+		li t3,-110
+		beq t2,t3,FIM # se o pixel for azul ele n„o se meche
+		li t3,63
+		beq t2,t3,SegundaParte # se for amarelo ele vai para a segunda parte do mapa
+		lh t2, 0(t5)
+		addi t2, t2,4
+		sh t2, 0(t5)
+		lh t2, 2(t5)
+		addi t2, t2,-4
+		sh t2, 2(t5)
+		ret
 	
-	lw t1, 0(a0)
-	lw t2, 4(a0)
-	li t3, 0
-	li t4, 0
-	addi a0,a0,8
-	While_LS:	
-		beq t2, t4, EndWhile_LS
-		add t0, t0,a1
-		While_LS1:
-			beq t1, t3, EndWhile_LS1
-			lw t5, 0(a0)
-			sw t5, 0(t0)
-			addi t3,t3,4
-			addi a0,a0,4
-			addi t0,t0,4
-			j While_LS1
-		EndWhile_LS1:
-		li t5, 320
-		sub t5, t5, a1
-		sub t5, t5,t1
-		add t0, t0, t5
-		li t3, 0
-		addi t4, t4,1
-		j While_LS
-	EndWhile_LS:
-	ret
-	
-UnloadImage:						# a0= endereco imagem
-	li t0, 76800					# a1 = x da imagem
-	mul t0, t0, a3					# a2 = y da imagem
-	la t1 , array_layers				# a3 = layer(0,5)
-	add t0 , t0, t1
-	li t1 , 320
-	mul t1,a2,t1
-	add t0,t0,t1					
- 	lw t2, 0(a0)
-	lw t3, 4(a0)
-	li t4, 0
-	li t5, 0
-	li t6 ,0xC7C7C7C7
-	While_UI:	
-		beq t3, t4, EndWhile_UI
-		add t0, t0,a1
-		While_UI1:
-			beq t2, t5, EndWhile_UI1
-			sw t6, 0(t0)
-			addi t5,t5,4
-			addi t0,t0,4
-			j While_UI1
-		EndWhile_UI1:
-		li t5, 320
-		sub t5, t5, a1
-		sub t5, t5,t2
-		add t0, t0, t5
-		li t5, 0
-		addi t4, t4,1
-		j While_UI
-	EndWhile_UI:					
- 	ret						
+	MoveUpLeft:
+		addi t6,t6,-1284
+		
+		lb t4,0(t6)
+		li t6,-110
+		beq t4,t6,FIM
 
-LoadImage:						# a0= endereco imagem
-	li t0, 76800					# a1 = x da imagem
-	mul t0, t0, a3					# a2 = y da imagem
-	la t1 , array_layers				# a3 = layer(0,5)
-	add t0 , t0, t1	
-	li t1 , 320
-	mul t1,a2,t1
-	add t0,t0,t1				
- 	lw t2, 0(a0)
-	lw t3, 4(a0)
-	li t4, 0
-	li t5, 0
-	addi a0,a0,8
-	While_LI:	
-		beq t3, t4, EndWhile_LI
-		add t0, t0,a1
-		While_LI1:
-			beq t2, t5, EndWhile_LI1
-			lw t6, 0(a0)
-			sw t6, 0(t0)
-			addi t5,t5,4
-			addi a0,a0,4
-			addi t0,t0,4
-			j While_LI1
-		EndWhile_LI1:
-		li t5, 320
-		sub t5, t5, a1
-		sub t5, t5, t2
-		add t0, t0, t5
-		li t5, 0
-		addi t4, t4,1
-		j While_LI
-	EndWhile_LI:
-	ret	
-
-Renderizador:
-	lw t0, selectframeads	
-	lb t0, 0(t0)					#a0 = x0
-	xori t0,t0,1					#a1 = y0
-	li t1, 0xFF0					#a2 = x1
-	add t0 ,t0,t1					#a3 = y1
-	slli t0 ,t0, 20					#a4 = atualizar tela ou nao				
-	la t6, array_layers				#x1 < x2, y1 < y2
-	li t5 , 320					#t0 eh o endereco da tela
-	mul t5 , t5, a1					#t1/t2/t3/t4 sao os contadores
-	add t0, t5 , t0					#t5 eh usado como valor temporario a todo momento
-	add t6, t5,t6					#t6 eh o endereco na memoria
-	sub t1,a3,a1					#ESTOU USANDO a7 e a6 COMO TEMP
-	sub t2,a2,a0
-	li t3, 0
-	li t4, 0
-	li a6, 0xffffffC7
-	While_R:	
-		beq t1, t4, EndWhile_R
-		add t0, t0,a0
-		add t6, t6,a0
-		While_R1:
-			beq t2, t3, EndWhile_R1
-			add a7, t6, zero
-			li t5,384000
-			add a7, a7,t5
-			While_R2:
-				lb t5,0(a7)
-				bne t5, a6,EndWhile_R2
-				li t5 , 268577812
-				blt a7,t5,EndWhile_R3
-				li t5, 76800
-				sub a7, a7,t5
-				j While_R2
-			EndWhile_R3:
-			li t5, 0xc7
-			EndWhile_R2:
-			sb t5, 0(t0)
-			addi t3,t3,1
-			addi t0,t0,1
-			addi t6,t6,1
-			j While_R1
-		EndWhile_R1:
-		li t5, 320
-		sub t5, t5, a2
-		add t0, t0, t5
-		add t6,t6,t5
-		li t3, 0
-		addi t4, t4,1
-		j While_R
-	EndWhile_R:
-	beq a4, zero , Fim_R
-	lw t0, selectframeads	
-	lb t1, 0(t0)			
-	xori t1,t1,1
-	sb t1,0(t0) 
-	Fim_R:ret
+		lh t1, 2(t5)
+		addi t1, t1, -4 
+		sh t1, 2(t5)
+		lh t1, 0(t5)
+		addi t1, t1, -4 
+		sh t1, 0(t5)
+		ret
 	
-FimPrograma:		#Nao recebe nada
-	li a7,10      	#Chama o procedimento de finalizar o programa
-	ecall		#Nao retorna nada
+	MoveDownLeft:
+		addi t6,t6,1276
+		
+		lb t4,0(t6)
+		li t6,-110
+		beq t4,t6,FIM
+
+		lh t1, 2(t5)
+		addi t1, t1, 4 
+		sh t1, 2(t5)
+		lh t1, 0(t5)
+		addi t1, t1, -4 
+		sh t1, 0(t5)
+		ret
+.include "data/funcoes.asm"
