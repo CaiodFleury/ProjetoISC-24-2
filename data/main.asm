e6a698a9d6ae71e4d55f43d06ce4869b18fa4171
OMassinha <caiodiasfleury@gmail.com>
2025-02-20 12:31:50 -0300

    Update main.asm

--- data/main.asm
+++ data/main.asm
@@ -35,8 +35,15 @@ main:
 	mv s0, a0
 	bltu s0, s4, FimAux
 	
-	call LoadGame
-	#call FimPrograma
+	li a0,0xFF200000
+	sw zero,0(a0)
+	Esperar_Leitura_mainl:
+	li a0,0xFF200000		# carrega o endereco de controle do KDMMIO
+	lw t0,0(a0)			# Le bit de Controle Teclado
+	andi t0,t0,0x0001		# mascara o bit menos significativo
+   	beq t0,zero,Esperar_Leitura_mainl
+
+	call FimPrograma
 	
 #Administrador maximo do jogo
 LoadGame:
